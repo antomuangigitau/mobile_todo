dd339d1bd766c82e469d63e7e28dce24893f9e2f
Antony <gitaumuangi@gmail.com>
2025-01-06 15:27:08 +0300

    Set up bottom tabs navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { theme } from "../theme";
 import ShoppingListItem from "../components/ShoppingListItem";
 import { useRouter } from "expo-router";
 
 export default function App() {
-  const router = useRouter();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.navigate("/counter")}>
-        <Text style={{ textAlign: "center", marginBottom: 20 }}>
-          Go to Counter
-        </Text>
-      </TouchableOpacity>
       <ShoppingListItem name="Tea" />
       <ShoppingListItem name="Coffee" isCompleted />
       <ShoppingListItem name="Sugar" isCompleted />
