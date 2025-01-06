c61c11fc6492a8abe55c56b62ef38a690f3b0b89
Antony <gitaumuangi@gmail.com>
2025-01-06 11:47:26 +0300

    Install and configure expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { theme } from "./theme";
-import ShoppingListItem from "./components/ShoppingListItem";
+import { theme } from "../theme";
+import ShoppingListItem from "../components/ShoppingListItem";
 
 export default function App() {
   return (
